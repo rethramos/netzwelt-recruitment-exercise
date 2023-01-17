6c135c40a004623ff3b8c0346727a43e750e3111
Rethaniel Ramos <rethanielramos@gmail.com>
2023-01-17 22:46:15 +0800

    create PageController

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PageController::class, 'home']);
+Route::get('home/index', [PageController::class, 'home']);
