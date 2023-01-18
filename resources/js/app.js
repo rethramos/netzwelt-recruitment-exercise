9d3ceb2aa4cae9e7be185e3287c6e2a72557092b
Rethaniel Ramos <rethanielramos@gmail.com>
2023-01-18 17:46:29 +0800

    create base layout

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 import './bootstrap';
+import { createApp } from 'vue/dist/vue.esm-bundler';
+import ToggleListItem from './components/ToggleListItem.vue'
+
+const app = createApp()
+
+app.component('toggle-list-item', ToggleListItem)
+
+app.mount('#app')
