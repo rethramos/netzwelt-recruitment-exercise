4f1dc4664b6749cb3c396a717ee4dbbd266203f6
Rethaniel Ramos <rethanielramos@gmail.com>
2023-01-18 16:48:56 +0800

    restructure territory data into tree form

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,11 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class PageController extends Controller
 {
-    //
     public function home() {
-        return view('page.home');
+        $response = Http::withOptions(['base_uri' => env('NETZWELT_URL')])
+                    ->get('/Territories/All');
+        $data = $response->json()['data'];
+
+        $dataLookup = [];
+        $roots = [];
+        for ($i=0; $i < count($data); $i++) { 
+            $el = $data[$i];
+            $el['children'] = [];
+            $dataLookup[$el['id']] = $el;
+        }
+
+        foreach ($data as $key => $value) {
+            if($value['parent'] == null) {
+                $roots[] = &$dataLookup[$value['id']];
+            } else {
+                $dataLookup[$value['parent']]['children'][] = &$dataLookup[$value['id']];
+            }
+        } 
+        
+        return view('page.home', ['roots' => $roots]);
     }
 }
