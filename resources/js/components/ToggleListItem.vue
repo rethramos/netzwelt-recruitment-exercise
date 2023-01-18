26a84fcb22a41da2782d0ef3dd23bd922db79837
Rethaniel Ramos <rethanielramos@gmail.com>
2023-01-18 18:05:29 +0800

    render territory list

--- resources/js/components/ToggleListItem.vue
+++ resources/js/components/ToggleListItem.vue
@@ -1,9 +1,20 @@
 <template>
-    <div>hello</div>
+    <li v-for="node in treeNodes" :key="node.id">
+      <span>{{ node.name }}</span>
+      <ul v-if="node.children.length > 0">
+        <toggle-list-item :tree-nodes="node.children"/>
+      </ul> 
+    </li>
 </template>
 <script>
 export default {
     name: "toggle-list-item",
+    props: {
+      treeNodes: {
+        type: Array,
+        required: true
+      }
+    }
 };
 </script>
 <style></style>
--- resources/views/page/home.blade.php
+++ resources/views/page/home.blade.php
@@ -1,1 +1,9 @@
-<h1>test</h1>
+@extends('layouts.app')
+@section('content')
+  <h1>Territories</h1>
+  <p>Here are the list of territories</p>
+
+  <ul>
+    <toggle-list-item :tree-nodes="{{json_encode($roots)}}"/>
+  </ul>
+@endsection
