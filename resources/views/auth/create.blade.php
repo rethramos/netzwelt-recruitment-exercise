143a2cdbbd8745b3466f50fddc56d6e618f81871
Rethaniel Ramos <rethanielramos@gmail.com>
2023-01-18 23:34:05 +0800

    use Bootstrap

--- resources/views/auth/create.blade.php
+++ resources/views/auth/create.blade.php
@@ -1,18 +1,20 @@
 @extends('layouts.app')
 @section('content')
-  <form action="{{ route('auth.store') }}" method="post">
-    @isset($message)
-      <span>{{$message}}</span>
-    @endisset
-    @csrf
-    <div>
-      <label for="username">Username</label>
-      <input type="text" name="username" id="username">
-    </div>
-    <div>
-      <label for="password">Password</label>
-      <input type="password" name="password" id="password">
-    </div>
-    <button type="submit">Log in</button>
-  </form>
+  <div class="card">
+    <form action="{{ route('auth.store') }}" method="post" class="card-body">
+      @isset($message)
+        <div class="alert alert-danger" role="alert">{{$message}}</div>
+      @endisset
+      @csrf
+      <div class="mt-2">
+        <label for="username" class="form-label">Username</label>
+        <input type="text" name="username" id="username" class="form-control">
+      </div>
+      <div class="mt-2">
+        <label for="password" class="form-label">Password</label>
+        <input type="password" name="password" id="password" class="form-control">
+      </div>
+      <button type="submit" class="btn btn-primary mt-2 float-end">Log in</button>
+    </form>
+  </div>
 @endsection
